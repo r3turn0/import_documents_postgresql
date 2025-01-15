2fcfaeb6eb153a037330308a6b515ffa04eeefe5
r3turn0 <jericta87@gmail.com>
2025-01-15 13:46:03 -0800

    fixed email components

--- documents_import_script.js
+++ documents_import_script.js
@@ -34,16 +34,16 @@ let mailOptions = {
     // html: '<b>Hello world?</b>'                 // HTML body
 };
 
-// Function to import files into the database etc.documents table but first selects the uid of the user who uploaded the file from the etc.users table
+// Function to import files into the database etc.upload table but first selects the uid of the user who uploaded the file from the etc.user table
 async function importDocuments(file) {
     const firstName = process.env.UPLOADED_BY.split(' ')[0];
     const lastName = process.env.UPLOADED_BY.split(' ')[1];
     console.log('Uploading file:', file, 'by',firstName, lastName);
-    const getUserQuery = `SELECT uid FROM etc.users WHERE first_name LIKE $1 AND last_name LIKE $2`;
+    const getUserQuery = `SELECT uid FROM etc.user WHERE first_name LIKE $1 AND last_name LIKE $2`;
     const resultUser = await client.query(getUserQuery, [firstName, lastName]);
     const resultUserId = resultUser.rows[0].uid;
     console.log('User: ', resultUser);
-    const getFileQuery = `SELECT load_id, date_upload FROM etc.products WHERE filename LIKE '%' || $1 || '%'`;
+    const getFileQuery = `SELECT load_id, date_upload FROM etc.product WHERE filename LIKE '%' || $1 || '%'`;
     const resultFile = await client.query(getFileQuery, [file]);
     if (resultFile.rows.length === 0) {
         console.log('No file found in the database matching:', file);
@@ -52,24 +52,38 @@ async function importDocuments(file) {
     const load_id = resultFile.rows[0].load_id;
     const date_upload = resultFile.rows[0].date_upload;
     console.log('Load ID: ', load_id);
-    const query = `INSERT INTO etc.documents (filename, date_upload, uid, uploaded_by, filepath, load_id) 
+    const query = `INSERT INTO etc.upload (filename, date_upload, uid, uploaded_by, filepath, load_id) 
                     VALUES ($1, $2, $3, $4, $5, $6)`;
-    const values = [file, date_upload, resultUserId, process.env.UPLOADED_BY, directoryPath + '/' + file, load_id];
+    const values = [file, date_upload, resultUserId, process.env.UPLOADED_BY, process.env.DESTINATION_PATH + '\\' + file, load_id];
     const results = await client.query(query, values);
     console.log('Row inserted: ', results);
     const fileName = file;
     const uploaded_by = process.env.UPLOADED_BY;
-    const subject = 'etc.documents IMPORT: ' + fileName + ' on ' + date_upload;
-    let text = 'Hello ' + firstName + ' id: ' + resultUserId + ',\n\n' + load_id + ' ' + fileName + ' was successfully imported into the etc.documents table on ' + date_upload + '.\n\n' + 'Regards,\n' + process.env.UPLOADED_BY;
-    let html = '<h1> Document file Import</h1><p>' + text + '</p>';
-    const emailObj = {
-        date: date_upload,
-        uuid: resultUserId,
-        load_id: load_id,
-        filename: fileName,
-        uploaded_by: uploaded_by,
-        subject: subject
-    }
+    const subject = 'IMPORT to etc.upload: ' + fileName + ' on ' + date_upload;
+    let text = 'Hello ' + firstName + ' ' + lastName + ' uid: ' + resultUserId + ', File load_id: ' + load_id + ' with filename: ' + fileName + ' was successfully imported into the etc.upload table on ' + date_upload + '. Regards, \n' + uploaded_by;
+    html = '<h1>Uploaded File</h1><p>' + 
+        text + '</p>' + 
+        '<p> Load Id: ' + 
+        load_id  + 
+        '</p>' + 
+        '<p> Filename: ' + 
+        fileName  + 
+        '</p>' + 
+        '<p> Uploaded By: ' + 
+        uploaded_by  + 
+        '</p>' + 
+        '<p> Date Upload: ' + 
+        date_upload  + 
+        '</p>';
+    ;
+    // const emailObj = {
+    //     date: date_upload,
+    //     uuid: resultUserId,
+    //     load_id: load_id,
+    //     filename: fileName,
+    //     uploaded_by: uploaded_by,
+    //     subject: subject
+    // }
     mailOptions.subject = subject;
     mailOptions.text = text;
     mailOptions.html = html;
@@ -95,6 +109,13 @@ fs.readdir(directoryPath, (err, files) => {
             console.log('Processing file: ', file);
             await importDocuments(file);
             console.log('File imported: ', file);
+            // move file to destination path
+            fs.rename(directoryPath + '\\' + file, process.env.DESTINATION_PATH + '\\' + file, function(err) {
+                if (err) {
+                    console.log('Error renaming file: ', err);
+                }
+                console.log('File moved to destination path: ', process.env.DESTINATION_PATH + '\\' + file);
+            });
         }
         await client.end();
         console.log('Connection ended: ', client.host, client.database, client.port, client.user, client.password);
